eef9dbf49cdd37537a1d7c491aab678e7a8e424a
Hugo Danet <hugo.danet@telecom-paris.fr>
2021-01-17 11:55:00 +0100

    Used Hook to fix tab bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,6 +19,43 @@ import Tab from "@material-ui/core/Tab";
 import {Link} from 'react-router-dom';
 import './navbar.css';
 
+// Hook
+function useLocalStorage(key, initialValue) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = React.useState(() => {
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = value => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
 const useStyles = makeStyles((theme) => ({
   grow: {
     flexGrow: 1,
@@ -92,7 +129,7 @@ export default function NavBar() {
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useLocalStorage("value", 0);
 
   const handleChangeTabs = (event, newValue) => {
     setValue(newValue);
