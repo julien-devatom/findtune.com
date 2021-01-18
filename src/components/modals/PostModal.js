4d6754f49f4047e4a9349cd7184942602dcfd700
Julien <julien.thomas84@gmail.com>
2021-01-18 15:47:46 +0100

    postModal

--- src/components/modals/PostModal.js
+++ src/components/modals/PostModal.js
@@ -1,20 +1,19 @@
 import {useState} from "react";
-import {Dialog} from "material-ui";
-import {Button, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@material-ui/core";
+import {useForm} from "react-hook-form";
 
 
 export default function PostModal({_open}){
+    const {register, handleSubmit, formState, errors} = useForm();
+    const {isSubmitting} = formState
     const [open, setOpen] = useState(_open)
-    const handleClose = () =>{
-    }
-    const handleSubmit = () => {
-
+    const onSubmit = data => {
+        console.log(data)
     }
     return(
         <div className="modal fade" id="postModal" tabIndex="-1" role="dialog"
              aria-hidden="false">
             <div className="modal-dialog" role="document">
-                <form onSubmit={handleSubmit}>
+                <form  onSubmit={handleSubmit(onSubmit)} >
                 <div className="modal-content">
                     <div className="modal-header">
                         <h5 className="modal-title" id="exampleModalLabel">Add a post</h5>
@@ -25,21 +24,21 @@ export default function PostModal({_open}){
                     <div className="modal-body">
                         <div className="form-group">
                             <label htmlFor="title">Title</label>
-                            <input name="title" id="title" className="form-control w-100" />
+                            <input name="title" id="title" className="form-control w-100" ref={register}/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="content">Content</label>
-                            <textarea name="content" id="content" className="form-control w-100" />
+                            <textarea name="content" id="content" className="form-control w-100" ref={register} />
                         </div>
                         <div className="form-group">
                             <label htmlFor="video_url">Add a video url</label>
-                            <input name="video_url" id="video_url" className="form-control w-100" />
+                            <input name="video_url" id="video_url" className="form-control w-100" ref={register}/>
                         </div>
 
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-primary mr-auto" data-dismiss="modal">Close</button>
-                        <button type="submit" className="btn btn-success">Add a post</button>
+                        <button type="submit" className="btn btn-success" disabled={isSubmitting}>Add a post</button>
                     </div>
                 </div>
                 </form>
